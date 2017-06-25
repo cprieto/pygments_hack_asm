a78904a32e903935ff7acd8c738c647ef7e60eab
Cristian Prieto <me@cprieto.com>
2017-06-26 09:15:36 +1000

    Time for the PyPI package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygments-hackasm-lexer',
+      version='0.1',
+      description='Pygments lexer for the Nand2Tetris Hack Assembler',
+      url='https://github.com/cprieto/pygments_hack_asm',
+      author='Cristian Prieto',
+      author_email='me@cprieto.com',
+      license='MIT',
+      classifiers =[])
+
