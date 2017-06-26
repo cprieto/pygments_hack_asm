ba2274fca3ff05a41e3927189998b921e7b0ddc4
Cristian Prieto <me@cprieto.com>
2017-06-26 11:34:01 +1000

    Fix issue with setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='pygments-hackasm-lexer',
       version='0.1',
       description='Pygments lexer for the Nand2Tetris Hack Assembler',
-      packages = setuptools.find_packages(),
+      packages = find_packages(),
       url='https://github.com/cprieto/pygments_hack_asm',
       author='Cristian Prieto',
       author_email='me@cprieto.com',
