07ccbc36fd5148db2efc5f676fd13d4b24aa004f
Cristian Prieto <me@cprieto.com>
2017-06-26 17:37:41 +1000

    Add register and IO addresses

--- hackasmlexer/hacklexer.py
+++ hackasmlexer/hacklexer.py
@@ -19,6 +19,9 @@ class HackAsmLexer(RegexLexer):
             (r'[\r\n]+', Text),
             (r'@[A-Za-z][A-Za-z0-9]+', Name.Variable),
             (r'\b(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)\b', Keyword),
+            (r'\b@(SCREEN|KBD)\b', Name.Builtin.Pseudo), # I/O addresses
+            (r'\b@(R0|R1|R2|R3|R4|R5|R6|R7|R8|R9|R10|R11|R12|R13|R14|R15)\b', Name.Builtin.Pseudo), # RAM Addresses
+            (r'\b@(SP|LCL|ARG|THIS|THAT)\b', Name.Builtin.Pseudo), # Parameter addresses
             (r'null', Keyword.Pseudo),
             (r'\b(D|M|MD|A|AM|AD|AMD)\b', Name.Builtin),
             (r'@[0-9]+', Name.Constant)
