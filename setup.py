8f32c1563123e126619815094c258578d650b703
Cristian Prieto <me@cprieto.com>
2017-06-26 10:40:55 +1000

    Add metadata file and keywords

--- setup.py
+++ setup.py
@@ -7,5 +7,20 @@ setup(name='pygments-hackasm-lexer',
       author='Cristian Prieto',
       author_email='me@cprieto.com',
       license='MIT',
-      classifiers =[])
+      keywords = [
+          'syntax highlighting',
+          'pygments',
+          'lexer',
+          'hack',
+          'assembler',
+          'nand2tetris'],
+      classifiers =[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',
+          'Environment :: Plugins'])
 
